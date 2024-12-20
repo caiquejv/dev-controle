5d443db8c4e4a2e8357473c01e868a33bba2a4da
Caíque Villela <caique.villela@hotmail.com>
2024-12-19 23:18:26 -0300

    feat: Add more components and configure user authentication

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from '@/components/container'
+
+export default function Dashboard() {
+    return (
+        <Container>
+          <h1>ddddd</h1>
+        </Container>
+    )
+}
