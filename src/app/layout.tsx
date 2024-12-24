161b1aeebdfddaddc14d35be1506475dec5ee12d
Caíque Villela <caique.villela@hotmail.com>
2024-12-23 22:00:24 -0300

    feat: finalize authentication with NextAuth

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,39 +1,29 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Header } from "@/components/Header"
-import { AuthProvider } from "@/providers/auth"
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { Header } from '@/components/header'
+import { AuthProvider } from '@/providers/auth'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: "Dev Controle - Seu sistema de gerenciamento.",
-  description: "Gerencie seus clientes e atendimentos de forma eficiente.",
-};
+  title: 'Dev Controle - Seu sistema de gerencimaneto.',
+  description: 'Gerencie seus clientes e atendimentos de forma fácil!',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: {
+  children: React.ReactNode
+}) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={inter.className}>
         <AuthProvider>
-        <Header />
-        {children}
+          <Header />
+          {children}
         </AuthProvider>
       </body>
     </html>
-  );
+  )
 }
--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,18 +1,20 @@
-import {PrismaClient} from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
 let prisma: PrismaClient;
 
-let globalWithPrisma = global as typeof globalThis & { prisma?: PrismaClient };
-
-if (process.env.NODE_ENV === 'production') {
+if(process.env.NODE_ENV === "production"){
   prisma = new PrismaClient();
-} else {
-  if (!globalWithPrisma.prisma) {
+}else{
+  let globalWithPrisma = global as typeof globalThis & {
+    prisma: PrismaClient;
+  }
+
+  if(!globalWithPrisma.prisma){
     globalWithPrisma.prisma = new PrismaClient();
   }
+
+  prisma = globalWithPrisma.prisma;
+
 }
 
-prisma = globalWithPrisma.prisma;
-
-
-export default prisma;	
+export default prisma;
